5d23276879157bffbf4726945271d6aeee65c684
peter <pkarella27@yahoo.com>
2017-06-02 14:43:01 -0700

    broken

--- app/components/inquiry-tile.js
+++ app/components/inquiry-tile.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  addNewInquiry: false,
+  actions: {
+    inquiryFormShow() {
+      this.set('addNewInquiry', true);
+    },
+    saveInquiry() {
+          var params = {
+            author: this.get('author'),
+            greatQuestion: this.get('greatQuestion'),
+            notes: this.get('notes'),
+          };
+          this.set('addNewInquiry', false);
+          this.sendAction('saveInquiry', params);
+        }
+      }
+    });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('inquiry');
  },
+ actions: {
+    saveInquiry(params) {
+      var newInquiry = this.store.createRecord('inquiry', params);
+      newInquiry.save();
+    }
+  }
 });
